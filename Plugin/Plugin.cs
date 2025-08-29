64f1ac79551e4302bcd58b5445e917090b30a273
art0007i <art0007i@gmail.com>
2025-08-29 18:45:19 +0200

    Add project files.

---
+++ Plugin/Plugin.cs
@@ -0,0 +1,54 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.NET.Common;
+using BepInExResoniteShim;
+using FrooxEngine;
+using HarmonyLib;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace RenderiteHook;
+
+[ResonitePlugin(PluginMetadata.GUID, PluginMetadata.NAME, PluginMetadata.VERSION, PluginMetadata.AUTHORS, PluginMetadata.REPOSITORY_URL)]
+[BepInDependency(BepInExResoniteShim.PluginMetadata.GUID, BepInDependency.DependencyFlags.HardDependency)]
+public class Plugin : BasePlugin
+{
+    internal static new ManualLogSource Log;
+
+    public override void Load()
+    {
+        Log = base.Log;
+
+        HarmonyInstance.PatchAll();
+    }
+
+    [HarmonyPatch(typeof(RenderSystem), "StartRenderer", MethodType.Async)]
+    public class ArgumentsPatch
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codes)
+        {
+            var patched = false;
+            foreach (var code in codes)
+            {
+                yield return code;
+                if(code.operand is MethodInfo mi && mi.Name == "ToStringAndClear")
+                {
+                    patched = true;
+                    yield return new CodeInstruction(OpCodes.Call, typeof(ArgumentsPatch).GetMethod(nameof(OnStartRenderer), BindingFlags.Static | BindingFlags.Public)); // Call our method
+                }
+            }
+
+            if (!patched)
+            {
+                Log.LogError("Failed to patch StartRenderer arguments!");
+            }
+        }
+
+        public static string OnStartRenderer(string args)
+        {
+            var newArgs = string.Join(' ', [args, ..Environment.GetCommandLineArgs().Skip(1)]);
+            Log.LogInfo($"Starting renderer with args: {newArgs}");
+            return newArgs;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# RenderiteHook
+[![Thunderstore Badge](https://gist.githubusercontent.com/art0007i/c4871bbdb30d31e7899328754916bb81/raw/076910e4939e624f17c88bd879770d3bd2fe3f1e/available-on-thunderstore.svg)](https://thunderstore.io/c/resonite/)
+
+A [Resonite](https://resonite.com/) mod that [TODO: describe what your mod does here].
+
+## Installation (Manual)
+1. Install [BepisLoader](https://github.com/ResoniteModding/BepisLoader) for Resonite.
+2. Download the latest release ZIP file (e.g., `ResoniteModding-RenderiteHook-1.0.0.zip`) from the [Releases](https://github.com/ResoniteModding/RenderiteHook/releases) page.
+3. Extract the ZIP and copy the `plugins` folder to your BepInEx folder in your Resonite installation directory:
+   - **Default location:** `C:\Program Files (x86)\Steam\steamapps\common\Resonite\BepInEx\`
+4. Start the game. If you want to verify that the mod is working you can check your BepInEx logs.
